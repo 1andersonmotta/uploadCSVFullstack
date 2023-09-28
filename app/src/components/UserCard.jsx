c1bac6fd170d18d0896b9fcf247c5628f3b3a3c9
1andersonmotta <devandersonmotta@gmail.com>
2023-09-28 13:43:25 -0300

    feature adjust in usercard

--- app/src/components/UserCard.jsx
+++ app/src/components/UserCard.jsx
@@ -1,25 +1,29 @@
 /* eslint-disable react/prop-types */
-import { FaStar } from 'react-icons/fa'
-import Movie from '../pages/Movie';
-// import { Link } from 'react-router-dom';
+import { BiMedal } from 'react-icons/bi'
+import { useNavigate } from 'react-router-dom';
+import '../pages/UserDetails.css'
 
 
-const UserCard = ({ user }) => {
-    console.log('user card', typeof (user))
-    const { name, city, country, favorite_sport } = user
-    const obj = { ...user }
-    console.log('obj', obj);
+const UserCard = ({ user, showLink = true }) => {
+    const { id, name, city, country, favorite_sport } = user
+
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        console.log(id)
+        if (!id) return;
+
+        navigate(`/user?id=${id}`);
+    }
     return (
-        <div className="movie-card">
+        <div className="user-card">
             <h2>{name}</h2>
-            <h2>{city}</h2>
             <h2>{country}</h2>
+            <h2>{city}</h2>
             <p>
-                <FaStar /> {favorite_sport}
+                <BiMedal /> {favorite_sport}
             </p>
-            {/* {showLink && <Link to={{ pathname: `/user`, state: { objectData:  user  } }}>Detalhes</Link>} */}
-            {/* {<Link to={{ pathname: `/movie`, state: { objectData: obj } }}>Detalhes</Link>} */}
-            <Movie user={user} />
+            {showLink && <button onClick={handleSubmit}>Details</button>}
         </div>
     )
 };
