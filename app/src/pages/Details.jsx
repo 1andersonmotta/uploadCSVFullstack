0b31e9d5ead3335946d80f26d79d2f531b6b35ee
1andersonmotta <devandersonmotta@gmail.com>
2023-09-28 13:43:39 -0300

    feature adjust in details

---
+++ app/src/pages/Details.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import UserDetails from "./UserDetails";
+
+const searchURL = import.meta.env.VITE_SEARCH
+
+const Details = () => {
+    const [searchParams] = useSearchParams()
+
+    const [users, setUsers] = useState([])
+    const query = searchParams.get("id")
+
+    const getSearchedUsers = async (url) => {
+        console.log('url', url)
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log('data', data)
+        setUsers(data)
+    }
+    useEffect(() => {
+        const searchWithQueryURL = `${searchURL}=${query}`;
+        getSearchedUsers(searchWithQueryURL)
+    }, [query])
+
+    return (
+        <div >
+            {users.length === 0 && <p>Carregando...</p>}
+            {users.length > 0 && users.map((user) => <UserDetails key={user.id} user={user} />)}
+        </div>
+    )
+}
+
+export default Details;
