48c13b24472fb58db6dbe572ecad82e342d409a6
1andersonmotta <devandersonmotta@gmail.com>
2023-09-29 10:45:51 -0300

    feature format home.jsx

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,14 +1,11 @@
+import FileUpload from '../components/FileUpload';
 import './UserGrid.css'
 
 const Home = () => {
 
     return (
         <div className="container">
-            <h2 className="title">Upload CSV</h2>
-            <div className='divinput'>
-                <input className='input' type="file" accept="file/csv" name="csv" id="csv" />
-            </div>
-
+            <FileUpload />
         </div>
     )
 }
