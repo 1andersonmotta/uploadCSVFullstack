47aaa9f402e272c2a7e15964231b50a6b307b3a9
1andersonmotta <devandersonmotta@gmail.com>
2023-10-03 17:03:29 -0300

    refactor errorpage

--- app/src/pages/ErrorPage.jsx
+++ app/src/pages/ErrorPage.jsx
@@ -1,5 +1,10 @@
 import './UserGrid.css';
 
-const ErrorPage = () => { return (<div className='diverror'><h1 className="error">Erro 404! Usuário não encontrado</h1></div>) };
+const ErrorPage = () => {
+    return (
+        <div className='diverror'>
+            <h1 className="error">Erro 404! Usuário não encontrado</h1>
+        </div>)
+};
 
 export default ErrorPage;
